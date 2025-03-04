9aa7ba7dd7fa494045ccfd025426c4c3b16076dd
Bhargav Hira <bhargavprotim000hira@gmail.com>
2025-03-04 12:58:34 +0530

    [Bhargav] Add: Script for validating last name

--- user.py
+++ user.py
@@ -1,7 +1,7 @@
 import re
 
-# Use Case 1: Validate First Name
-first_name = input("Enter a valid First Name: ")
-while not (len(first_name) >= 3 and first_name[0].isupper()):
-    print("Invalid first name. It should start with a capital letter and have at least 3 characters.")
-    first_name = input("Enter a valid First Name: ")
+# Use Case 2: Validate Last Name
+last_name = input("Enter a valid Last Name: ")
+while not (len(last_name) >= 3 and last_name[0].isupper()):
+    print("Invalid last name. It should start with a capital letter and have at least 3 characters.")
+    last_name = input("Enter a valid Last Name: ")
